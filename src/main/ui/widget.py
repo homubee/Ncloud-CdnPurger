e1d1e052412844bf89eb084c6888110f9f990674
homubee <imzergking@gmail.com>
2023-04-06 17:38:49 +0900

    #15 Connect history api with ui

--- src/main/ui/widget.py
+++ src/main/ui/widget.py
@@ -4,8 +4,9 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QStandardItemModel, QStandardItem, QResizeEvent
 from PyQt5.QtWidgets import QWidget
 
-from util.systemUtils import SystemUtil
+from util.purgeUtils import GetCdnPlusPurgeHistoryList_QueryInfo, GetCdnPlusPurgeHistoryList_ApiHandler
 from util.qtUtils import QtUtil
+from ui.dialog import MessageDialog
 
 class Widget(QWidget):
     """ 
@@ -45,32 +46,71 @@ class PurgeHistoryWidget(Widget, QtUtil.loadUiClass("./res/ui/PurgeHistoryWidget
 
         self.show()
 
-        self.setTableUi()
-
-    def setTableUi(self):
         model = QStandardItemModel()
         model.setHorizontalHeaderLabels(["일시", "범위", "대상", "결과"])
-        model.appendRow([QStandardItem(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), QStandardItem("전체"), QStandardItem("test3\ntest4"), QStandardItem("success")])
 
-        # set text alignment
-        model.item(0,0).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-        model.item(0,1).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-        model.item(0,3).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setTableRow(model, int(cdnInstanceNoStr))
+        self.setTableSize()
 
-        self.tableView_historyList.setModel(model)
-
-        # resize row
+    def setTableSize(self):
+        # Resize row
         self.tableView_historyList.resizeRowsToContents()
 
-        # resize column
+        # Resize column
         self.setColumnSize()
+
+    def setTableRow(self, model: QStandardItemModel, cdnInstanceNo: int):
+
+        queryInfo = GetCdnPlusPurgeHistoryList_QueryInfo(cdnInstanceNo, None, "JSON")
+        apiHandler = GetCdnPlusPurgeHistoryList_ApiHandler(queryInfo)
+
+        # Exception handling
+        try:
+            statusCode, returnCode, returnMessage, responseData = apiHandler.callApi()
+        except Exception as e:
+            self.messageDialog = MessageDialog(self, 0, "[Error] ", str(e))
+            self.close()
+        else:
+            self.messageDialog = MessageDialog(self, 2, 
+                                               "Status Code : " + str(statusCode), 
+                                               "Return Code : " +  returnCode, 
+                                               "Return Message : " + returnMessage)
+
+            # Close widget when response is not succeeded.
+            if (statusCode != 200):
+                self.close()
+                return
+
+            cdnPlusPurgeHistoryList = responseData["cdnPlusPurgeHistoryList"]
+
+            for index, history in enumerate(cdnPlusPurgeHistoryList):
+                requestDate = history["requestDate"].replace("T", " ").split("+")[0]
+                isWholePurge = "전체" if history["isWholePurge"] else "개별"
+                targetFileList = history["targetDirectoryName"] or ",\n".join(history["targetFileList"])
+                purgeStatusName = history["purgeStatusName"]
+
+                # Add items
+                rowList = list()
+                rowList.append(QStandardItem(requestDate))
+                rowList.append(QStandardItem(isWholePurge))
+                rowList.append(QStandardItem(targetFileList))
+                rowList.append(QStandardItem(purgeStatusName))
+
+                model.appendRow(rowList)
+
+                # Set text alignment
+                model.item(index,0).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+                model.item(index,1).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+                model.item(index,3).setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+
+            self.tableView_historyList.setModel(model)
 
     def setColumnSize(self):
         col_width = self.tableView_historyList.width()
-        self.tableView_historyList.setColumnWidth(0, int(col_width*4/10))
-        self.tableView_historyList.setColumnWidth(1, int(col_width*1/10))
-        self.tableView_historyList.setColumnWidth(2, int(col_width*3/10))
-        self.tableView_historyList.setColumnWidth(3, int(col_width*2/10))
+        self.tableView_historyList.setColumnWidth(0, int(col_width*35/100))
+        self.tableView_historyList.setColumnWidth(1, int(col_width*10/100))
+        self.tableView_historyList.setColumnWidth(2, int(col_width*30/100))
+        self.tableView_historyList.setColumnWidth(3, int(col_width*20/100))
 
     def resizeEvent(self, a0: QResizeEvent) -> None:
         self.setColumnSize()
